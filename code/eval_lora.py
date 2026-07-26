da127d36604c4fed7aa936ca2564fc1adcf0780b
DinelyWellehewage <dinelywh78@gmail.com>
2026-07-26 11:54:32 +0200

    fix cmd line bug

--- code/eval_lora.py
+++ code/eval_lora.py
@@ -1,7 +1,11 @@
 import argparse
+import sys
+from pathlib import Path
+
+PROJECT_ROOT = Path(__file__).resolve().parents[1]
+sys.path.insert(0, str(PROJECT_ROOT))
 
 from src.inference import generate_samples
-
 
 def parse_args():
     parser = argparse.ArgumentParser(
